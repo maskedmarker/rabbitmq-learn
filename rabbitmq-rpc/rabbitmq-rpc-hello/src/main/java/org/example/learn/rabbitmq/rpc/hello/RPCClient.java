dfd1b71dc9bf293b56d21ac74d5e68e7b63a0842
maskedmarker <changjianghui0822@gmail.com>
2022-07-02 15:23:31 +0800

    rabbitmq rpc

--- rabbitmq-rpc/rabbitmq-rpc-hello/src/main/java/org/example/learn/rabbitmq/rpc/hello/RPCClient.java
+++ rabbitmq-rpc/rabbitmq-rpc-hello/src/main/java/org/example/learn/rabbitmq/rpc/hello/RPCClient.java
@@ -1,125 +1,71 @@
 package org.example.learn.rabbitmq.rpc.hello;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.UUID;
-import java.util.concurrent.TimeoutException;
-
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.ConsumerCancelledException;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
-import com.rabbitmq.client.ShutdownSignalException;
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.AMQP.BasicProperties;
-import com.rabbitmq.utility.BlockingCell;;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeoutException;
 
 
-public class RPCClient {
-
-    private final Map<String, BlockingCell<Object>> _contibuationMap = new HashMap<String, BlockingCell<Object>>();
-    private int correlationId = 0;
-    private Connection connection;
-    private Channel channel;
-    private String requestQueuename = "rpc_queue";
-    private String replyQueueName;
-    private DefaultConsumer _consumer;
+public class RPCClient implements AutoCloseable{
+    private final Connection connection;
+    private final Channel channel;
+    private String requestQueueName = "rpc_queue";
 
     public RPCClient() throws IOException, TimeoutException {
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(RabbitConfig.ip);
-        factory.setPort(RabbitConfig.port);
-        factory.setUsername(RabbitConfig.username);
-        factory.setPassword(RabbitConfig.password);
+        factory.setHost("localhost");
 
         connection = factory.newConnection();
         channel = connection.createChannel();
-
-        replyQueueName = channel.queueDeclare().getQueue();
-        _consumer = setupConsumer();
     }
 
-    public String call(String message) throws IOException, ShutdownSignalException, ConsumerCancelledException,
-            InterruptedException, TimeoutException {
-        if (_consumer == null)
-            return null;
-        BlockingCell<Object> k = new BlockingCell<Object>();
-        BasicProperties props;
-        synchronized (_contibuationMap) {
-            correlationId++;
-            String corrId = "" + correlationId;
-            props = new BasicProperties.Builder()
-                    .correlationId(corrId)
-                    .replyTo(replyQueueName)
-                    .build();
-            _contibuationMap.put(corrId, k);
+    public static void main(String[] argv) {
+        try (RPCClient fibonacciRpc = new RPCClient()) {
+            for (int i = 0; i < 32; i++) {
+                String i_str = Integer.toString(i);
+                System.out.println(" [x] Requesting fib(" + i_str + ")");
+                String response = fibonacciRpc.call(i_str);
+                System.out.println(" [.] Got '" + response + "'");
+            }
+        } catch (IOException | TimeoutException | InterruptedException e) {
+            e.printStackTrace();
         }
+    }
 
-        channel.basicPublish("", requestQueuename, props, message.getBytes());
+    public String call(String message) throws IOException, InterruptedException {
+        final String corrId = UUID.randomUUID().toString();
 
-        Object reply = k.uninterruptibleGet(4000);
-        if (reply instanceof ShutdownSignalException) {
-            ShutdownSignalException sig = (ShutdownSignalException) reply;
-            ShutdownSignalException wrapper = new ShutdownSignalException(
-                    sig.isHardError(), sig.isInitiatedByApplication(),
-                    sig.getReason(), sig.getReference());
-            wrapper.initCause(sig);
-            throw wrapper;
-        } else {
-            return (String) reply;
-        }
+        String replyQueueName = channel.queueDeclare().getQueue();
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish("", requestQueueName, props, message.getBytes(StandardCharsets.UTF_8));
+
+        final BlockingQueue<String> response = new ArrayBlockingQueue<>(1);
+
+        String ctag = channel.basicConsume(replyQueueName, true, (consumerTag, delivery) -> {
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response.offer(new String(delivery.getBody(), StandardCharsets.UTF_8));
+            }
+        }, consumerTag -> {
+        });
+
+        String result = response.take();
+        channel.basicCancel(ctag);
+        return result;
     }
 
     public void close() throws IOException {
         connection.close();
-    }
-
-    protected DefaultConsumer setupConsumer() throws IOException {
-        DefaultConsumer consumer = new DefaultConsumer(channel) {
-            @Override
-            public void handleShutdownSignal(String consumerTag,
-                                             ShutdownSignalException signal) {
-                synchronized (_contibuationMap) {
-                    for (Entry<String, BlockingCell<Object>> entry : _contibuationMap
-                            .entrySet()) {
-                        entry.getValue().set(signal);
-                    }
-                    _consumer = null;
-                }
-            }
-
-            @Override
-            public void handleDelivery(String consumerTag, Envelope envelope,
-                                       AMQP.BasicProperties properties, byte[] body)
-                    throws IOException {
-                synchronized (_contibuationMap) {
-                    String replyId = properties.getCorrelationId();
-                    BlockingCell<Object> blocker = _contibuationMap
-                            .get(replyId);
-                    _contibuationMap.remove(replyId);
-
-                    if (null != body) {
-                        String replyBoay = new String(body, "UTF-8");
-                        blocker.set(replyBoay);
-                    } else {
-                        blocker.set(body);
-                    }
-                }
-            }
-        };
-        channel.basicConsume(replyQueueName, true, consumer);
-        return consumer;
-    }
-
-    public static void main(String[] args) throws Exception {
-        RPCClient fibrpc = new RPCClient();
-        System.out.println("request fib(30)");
-        String response = fibrpc.call("30");
-        System.out.println("got " + response + ",");
-        fibrpc.close();
     }
 }
--- rabbitmq-rpc/rabbitmq-rpc-hello/src/main/java/org/example/learn/rabbitmq/rpc/hello/RPCServer.java
+++ rabbitmq-rpc/rabbitmq-rpc-hello/src/main/java/org/example/learn/rabbitmq/rpc/hello/RPCServer.java
@@ -1,46 +1,71 @@
 package org.example.learn.rabbitmq.rpc.hello;
 
-import com.rabbitmq.client.AMQP.BasicProperties;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.QueueingConsumer;
+
+import com.rabbitmq.client.*;
+
+import java.nio.charset.StandardCharsets;
 
 public class RPCServer {
+    private static final String RPC_QUEUE_NAME = "rpc_queue";
 
-    public static final String RPC_QUENE_NAME = "rpc_queue";
-
-    public static void main(String[] args) throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(RPCConfig.ip);
-        factory.setPort(RPCConfig.port);
-        factory.setUsername(RPCConfig.username);
-        factory.setPassword(RPCConfig.password);
-
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-        channel.queueDeclare(RPC_QUENE_NAME, false, false, false, null);
-        channel.basicQos(1);
-        QueueingConsumer consumer = new QueueingConsumer(channel);
-        channel.basicConsume(RPC_QUENE_NAME, false, consumer);
-        System.out.println("Awaiting RPCrequests");
-        while (true) {
-            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-            BasicProperties props = delivery.getProperties();
-            BasicProperties replyProps = new BasicProperties.Builder().correlationId(props.getCorrelationId()).build();
-            String message = new String(delivery.getBody());
-            int n = Integer.parseInt(message);
-            System.out.println("fib(" + message + ")");
-            String response = "" + fib(n);
-            channel.basicPublish("", props.getReplyTo(), replyProps, response.getBytes());
-            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-        }
-    }
-
-    private static int fib(int n) throws Exception {
+    private static int fib(int n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
         return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel()) {
+            channel.queueDeclare(RPC_QUEUE_NAME, false, false, false, null);
+            channel.queuePurge(RPC_QUEUE_NAME);
+
+            channel.basicQos(1);
+
+            System.out.println(" [x] Awaiting RPC requests");
+
+            Object monitor = new Object();
+            DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+                AMQP.BasicProperties replyProps = new AMQP.BasicProperties
+                        .Builder()
+                        .correlationId(delivery.getProperties().getCorrelationId())
+                        .build();
+
+                String response = "";
+
+                try {
+                    String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                    int n = Integer.parseInt(message);
+
+                    System.out.println(" [.] fib(" + message + ")");
+                    response += fib(n);
+                } catch (RuntimeException e) {
+                    System.out.println(" [.] " + e.toString());
+                } finally {
+                    channel.basicPublish("", delivery.getProperties().getReplyTo(), replyProps, response.getBytes(StandardCharsets.UTF_8));
+                    channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+                    // RabbitMq consumer worker thread notifies the RPC server owner thread
+                    synchronized (monitor) {
+                        monitor.notify();
+                    }
+                }
+            };
+
+            channel.basicConsume(RPC_QUEUE_NAME, false, deliverCallback, (consumerTag -> { }));
+            // Wait and be prepared to consume the message from RPC client.
+            while (true) {
+                synchronized (monitor) {
+                    try {
+                        monitor.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
     }
 }
 
